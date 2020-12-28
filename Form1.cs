4e74c827280e19090b4b6796ff1318dfd9a0f723
jeads <60721703+jeads92@users.noreply.github.com>
2020-12-28 11:34:37 -0500

    player can now reveal icons when the icon is clicked.

--- Form1.cs
+++ Form1.cs
@@ -60,6 +60,27 @@ namespace MatchingGameTutorial
             }
         }
 
+        /// <summary>
+        /// Every label's Click event is handled by this event handler
+        /// </summary>
+        /// <param name="sender">The label that was clicked</param>
+        /// <param name="e"></param>
+        private void label_Click(object sender, EventArgs e)
+        {
+            Label clickedLabel = sender as Label;
 
+            if (clickedLabel != null)
+            {
+                // If the clicked label is black, the player clicked
+                // an icon that's already been reveled --
+                // ignore that click
+                if (clickedLabel.ForeColor == Color.Black)
+                {
+                    return;
+                }
+
+                clickedLabel.ForeColor = Color.Black;
+            }
+        }
     }
 }
