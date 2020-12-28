bf9c08a0f85ef788f51de95f19d8f8df81488cf6
jeads <60721703+jeads92@users.noreply.github.com>
2020-12-28 12:09:03 -0500

    game keeps matching tiles on screen if chosen together.

--- Form1.cs
+++ Form1.cs
@@ -120,6 +120,17 @@ namespace MatchingGameTutorial
                 secondClicked = clickedLabel;
                 secondClicked.ForeColor = Color.Black;
 
+                // If the player clicked two matching icons, keep them
+                // black and reset firstClicked and secondClicked
+                // so the player can click another icon
+                if (firstClicked.Text == secondClicked.Text)
+                {
+                    firstClicked = null;
+                    secondClicked = null;
+                    return;
+                }
+
+
                 // If the player gets this far, the player
                 // clicked two different icons, so start the
                 // timer (which will wait .75 seconds
